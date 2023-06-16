3252bc95a1815abe2f1f9f54bc21d3be577b68ac
Abhijit Bahl <abhijit.bahl27@gmail.com>
2023-06-16 12:32:19 -0700

    test3

--- main.tf
+++ main.tf
@@ -25,21 +25,6 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.10.0"
-  
-  name     = "blog-autoscaling"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-}
-
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
